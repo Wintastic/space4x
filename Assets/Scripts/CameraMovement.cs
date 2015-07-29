748b9427113d17837c5575a3f91a0f81df4d28a6
Nick <nsegu038@uottawa.ca>
2015-07-29 19:49:36 -0400

    Camera properly clamped and camera controls changed

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour {
 
 	public float mouseSensitivity = 1.0f;
+	public float panSpeed = 1.0f;
 	public float zoomSpeed = 50.0f;
 	private Vector3 lastPosition;
 
@@ -15,23 +16,55 @@ public class CameraMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
+
+		panCamera ();
+		zoomCamera();
+		clampCamera();
+	}
+
+	void panCamera() {
+		float z = transform.position.z;
+
+		//Mouse panning
+		if (Input.GetMouseButtonDown(2)) {
 			lastPosition = Input.mousePosition;
 		}
-
-		if (Input.GetMouseButton(0)){
+		
+		if (Input.GetMouseButton(2)){
 			Vector3 delta = Input.mousePosition - lastPosition;
-			transform.Translate(delta.x * mouseSensitivity, delta.y * mouseSensitivity, 0);
+			transform.Translate(delta.x * mouseSensitivity * panSpeed, delta.y * mouseSensitivity * panSpeed, 0);
 			lastPosition = Input.mousePosition;
-			float x = Mathf.Clamp(transform.position.x, -200, 200);
-			float y = Mathf.Clamp(transform.position.y, 0, 100);
-			float z = Mathf.Clamp(transform.position.z, -200, 0);
-			transform.position = new Vector3(x, y, z);
 		}
 
+		//Arrow key panning
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.Translate(0, 1 * panSpeed, 0);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.Translate(0, -1 * panSpeed, 0);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Translate(-1 * panSpeed, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Translate(1 * panSpeed, 0, 0);
+		}
+
+		//Prevent changes in Z position
+		transform.position = new Vector3(transform.position.x, transform.position.y, z);
+	}
+
+	void zoomCamera(){
 		float scrollWheelDelta = Input.GetAxis("Mouse ScrollWheel");
 		if (scrollWheelDelta != 0f) {
 			transform.Translate(Vector3.forward*zoomSpeed*scrollWheelDelta);
 		}
+	}
+
+	void clampCamera(){
+		float x = Mathf.Clamp(transform.position.x, -200, 200);
+		float y = Mathf.Clamp(transform.position.y, 0, 100);
+		float z = Mathf.Clamp(transform.position.z, -200, 0);
+		transform.position = new Vector3(x, y, z);
 	}
 }
